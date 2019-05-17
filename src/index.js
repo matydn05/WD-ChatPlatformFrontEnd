878b9e59a0bf698077cebe9bd5c038e2e241733c
Matias Nunez <matydn05@gmail.com>
2019-05-17 17:36:34 -0300

    Delete db test

--- src/index.js
+++ src/index.js
@@ -5,21 +5,8 @@ import * as serviceWorker from './serviceWorker'
 import ApolloClient from 'apollo-boost'
 import { render } from 'react-dom'
 import { ApolloProvider } from 'react-apollo'
-import { gql } from 'apollo-boost'
 
 const client = new ApolloClient({ uri: 'http://localhost:3001/graphql' })
-
-client
-  .query({
-    query: gql`
-      {
-        user(id: "e89f5165-1fb6-4f41-a573-993b7712d745") {
-          username
-        }
-      }
-    `
-  })
-  .then(result => console.log(result))
 
 render(
   <ApolloProvider client={client}>
