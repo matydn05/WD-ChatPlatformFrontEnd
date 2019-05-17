50aacc53a9ea2d1356f0298e5f20163b18790646
Matias Nunez <matydn05@gmail.com>
2019-05-17 16:54:08 -0300

    Install react-router and code sign up form

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
 import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
-import ApolloClient from "apollo-boost"
-import { render } from "react-dom"
-import { ApolloProvider } from "react-apollo";
-import { gql } from "apollo-boost";
+import ApolloClient from 'apollo-boost'
+import { render } from 'react-dom'
+import { ApolloProvider } from 'react-apollo'
+import { gql } from 'apollo-boost'
 
-
-const client = new ApolloClient({uri: "http://localhost:3001/graphql"})
+const client = new ApolloClient({ uri: 'http://localhost:3001/graphql' })
 
 client
   .query({
@@ -21,16 +19,14 @@ client
       }
     `
   })
-  .then(result => console.log(result));
+  .then(result => console.log(result))
 
-  render(
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>,
-    document.getElementById("root")
-  );
-  
-  ReactDOM.render(<App />, document.getElementById('root'))
+render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
